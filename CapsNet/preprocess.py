a304df0d92744ca171bd16c0f6f8ef4354bb7b29
phaptrinh2000 <phaptrinh2000@gmail.com>
2021-07-18 22:11:39 +0700

    ud3

--- CapsNet/preprocess.py
+++ CapsNet/preprocess.py
@@ -1,6 +1,6 @@
 import yaml
 from data_process.data_process import data_process
 
-config = yaml.safe_load(open('/content/DS203/config.yml'))
+config = yaml.safe_load(open('/content/DS203.L21/CapsNet/config.yml'))
 
 data_process(config)
--- CapsNet/test.py
+++ CapsNet/test.py
@@ -2,7 +2,7 @@ import yaml
 import os
 from train.test import test
 
-config = yaml.safe_load(open('/content/DS203/config.yml'))
+config = yaml.safe_load(open('/content/DS203.L21/CapsNet/config.yml'))
 mode = config['mode']
 os.environ["CUDA_VISIBLE_DEVICES"] = str(config['aspect_' + mode + '_model'][config['aspect_' + mode + '_model']['type']]['gpu'])
 test(config)
